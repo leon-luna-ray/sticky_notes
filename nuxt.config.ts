1f66e25cb9498776e6dab2c455a01ee874b2ffeb
Ray Luna <leon.luna.ray@gmail.com>
2023-04-03 17:58:07 -0700

    Inital commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: ['@nuxtjs/supabase'],
+    supabase: {
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_KEY,
+        serviceKey: process.env.SUPABASE_SERVICE_KEY,
+    },
+})
